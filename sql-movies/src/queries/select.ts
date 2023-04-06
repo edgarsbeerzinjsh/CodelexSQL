14265408fe43227cac4bfb39ae5ec2385576540e
edgars.beerzinjsh <103140210+edgarsbeerzinjsh@users.noreply.github.com>
2023-04-06 10:35:59 +0300

    Movies 01 and 02 all pass

--- sql-movies/src/queries/select.ts
+++ sql-movies/src/queries/select.ts
@@ -1,57 +1,57 @@
 export const selectActorByName = (fullName: string): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM actors WHERE full_name = '${fullName}'`;
 };
 
 export const selectKeyword = (keyword: string): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM keywords WHERE keyword = '${keyword}'`;
 };
 
 export const selectDirector = (director: string): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM directors WHERE full_name = '${director}'`;
 };
 
 export const selectGenre = (genre: string): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM genres WHERE genre = '${genre}'`;
 };
 
 export const selectProductionCompany = (company: string): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM production_companies WHERE company_name = '${company}'`;
 };
 
 export const selectMovieById = (id: number): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM movies WHERE id = ${id}`;
 };
 
 export const selectGenreById = (id: number): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM genres WHERE id = ${id}`;
 };
 
 export const selectDirectorById = (id: number): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM directors WHERE id = ${id}`;
 };
 
 export const selectActorById = (id: number): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM actors WHERE id = ${id}`;
 };
 
 export const selectKeywordById = (id: number): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM keywords WHERE id = ${id}`;
 };
 
 export const selectProductionCompanyById = (id: number): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM production_companies WHERE id = ${id}`;
 };
 
 export const selectMovie = (imdbId: string): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM movies WHERE imdb_Id = '${imdbId}'`;
 };
 
 export const selectMovieId = (imdbId: string): string => {
-  throw new Error(`todo`);
+  return `SELECT id FROM movies WHERE imdb_Id = '${imdbId}'`;
 };
 
 export const selectRatingsByUserID = (userId: number): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM movie_ratings WHERE user_id = ${userId}`;
 };
 
 export const selectGenresByMovieId = (movieId: number): string => {
@@ -78,5 +78,5 @@ export const selectProductionCompaniesByMovieId = (movieId: number): string => {
  * select count as c, because an object is returned and expected property name is c
  */
 export const selectCount = (table: string): string => {
-  throw new Error(`todo`);
+  return `select count(*) as c from ${table}`; //?
 };
--- sql-movies/test/01-create-tables.test.ts
+++ sql-movies/test/01-create-tables.test.ts
@@ -11,31 +11,66 @@ import {
 } from "../src/table-names";
 import { tableInfo, indexList } from "../src/queries/table-info";
 
-const CREATE_MOVIES_TABLE = ``;
+const CREATE_MOVIES_TABLE = `CREATE TABLE ${MOVIES} (
+  id integer NOT NULL PRIMARY KEY,
+  imdb_id text NOT NULL,
+  popularity real NOT NULL,
+  budget real NOT NULL,
+  budget_adjusted real NOT NULL,
+  revenue real NOT NULL,
+  revenue_adjusted real NOT NULL,
+  original_title text NOT NULL,
+  homepage text,
+  tagline text,
+  overview text NOT NULL,
+  runtime integer NOT NULL,
+  release_date text NOT NULL
+)`;
 
-const CREATE_MOVIE_RATINGS_TABLE = ``;
+const CREATE_MOVIE_RATINGS_TABLE = `CREATE TABLE ${MOVIE_RATINGS} (
+  user_id integer NOT NULL,
+  movie_id integer NOT NULL,
+  rating real NOT NULL,
+  time_created text NOT NULL,
+  PRIMARY KEY (user_id, movie_id)
+)`;
 
-const CREATE_ACTORS_TABLE = ``;
+const CREATE_ACTORS_TABLE = `CREATE TABLE ${ACTORS} (
+  id integer NOT NULL PRIMARY KEY,
+  full_name text NOT NULL
+)`;
 
-const CREATE_KEYWORDS_TABLE = ``;
+const CREATE_KEYWORDS_TABLE = `CREATE TABLE ${KEYWORDS} (
+  id integer NOT NULL PRIMARY KEY,
+  keyword text NOT NULL
+)`;
 
-const CREATE_DIRECTORS_TABLE = ``;
+const CREATE_DIRECTORS_TABLE = `CREATE TABLE ${DIRECTORS} (
+  id integer NOT NULL PRIMARY KEY,
+  full_name text NOT NULL
+)`;
 
-const CREATE_GENRES_TABLE = ``;
+const CREATE_GENRES_TABLE = `CREATE TABLE ${GENRES} (
+  id integer NOT NULL PRIMARY KEY,
+  genre text NOT NULL
+)`;
 
-const CREATE_PRODUCTION_COMPANIES_TABLE = ``;
+const CREATE_PRODUCTION_COMPANIES_TABLE = `CREATE TABLE ${PRODUCTION_COMPANIES} (
+  id integer NOT NULL PRIMARY KEY,
+  company_name text NOT NULL
+)`;
 
-const CREATE_INDEX_MOVIES_RELEASE_DATE = ``;
+const CREATE_INDEX_MOVIES_RELEASE_DATE = `CREATE INDEX movies_release_date_idx ON ${MOVIES} (release_date)`;
 
-const CREATE_INDEX_MOVIE_RATINGS_TIME_CREATED = ``;
+const CREATE_INDEX_MOVIE_RATINGS_TIME_CREATED = `CREATE INDEX movie_ratings_time_created_idx ON ${MOVIE_RATINGS} (time_created)`;
 
-const CREATE_UNIQUE_INDEX_MOVIES_IMDB_ID = ``;
+const CREATE_UNIQUE_INDEX_MOVIES_IMDB_ID = `CREATE UNIQUE INDEX movies_imdb_id_unq_idx ON ${MOVIES} (imdb_id)`;
 
-const CREATE_UNIQUE_INDEX_KEYWORDS_KEYWORD = ``;
+const CREATE_UNIQUE_INDEX_KEYWORDS_KEYWORD = `CREATE UNIQUE INDEX keywords_keyword_unq_idx ON ${KEYWORDS} (keyword)`;
 
-const CREATE_UNIQUE_INDEX_GENRES_GENRE = ``;
+const CREATE_UNIQUE_INDEX_GENRES_GENRE = `CREATE UNIQUE INDEX genres_genre_unq_idx ON ${GENRES} (genre)`;
 
-const CREATE_UNIQUE_INDEX_PRODUCTION_COMPANIES_COMPANY_NAME = ``;
+const CREATE_UNIQUE_INDEX_PRODUCTION_COMPANIES_COMPANY_NAME = `CREATE UNIQUE INDEX production_companies_company_name_unq_idx ON ${PRODUCTION_COMPANIES} (company_name)`;
 
 describe("Tables", () => {
   let db: Database;
