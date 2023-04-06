42797f410cad71653303bfee7d028243ae526fff
edgars.beerzinjsh <103140210+edgarsbeerzinjsh@users.noreply.github.com>
2023-04-06 16:26:32 +0300

    Movies 04 pass

--- sql-movies/test/04-create-releationship-tables.test.ts
+++ sql-movies/test/04-create-releationship-tables.test.ts
@@ -10,15 +10,35 @@ import { Database } from "../src/database";
 import { tableInfo } from "../src/queries/table-info";
 import { minutes, Log } from "./utils";
 
-const CREATE_MOVIE_GENRES_TABLE = ``;
+const CREATE_MOVIE_GENRES_TABLE = `CREATE TABLE ${MOVIE_GENRES} (
+  movie_id integer NOT NULL,
+  genre_id integer NOT NULL,
+  PRIMARY KEY (movie_id, genre_id)
+)`;
 
-const CREATE_MOVIE_ACTORS_TABLE = ``;
+const CREATE_MOVIE_ACTORS_TABLE = `CREATE TABLE ${MOVIE_ACTORS} (
+  movie_id integer NOT NULL,
+  actor_id integer NOT NULL,
+  PRIMARY KEY (movie_id, actor_id)
+)`;
 
-const CREATE_MOVIE_DIRECTORS_TABLE = ``;
+const CREATE_MOVIE_DIRECTORS_TABLE = `CREATE TABLE ${MOVIE_DIRECTORS} (
+  movie_id integer NOT NULL,
+  director_id integer NOT NULL,
+  PRIMARY KEY (movie_id, director_id)
+)`;
 
-const CREATE_MOVIE_KEYWORDS_TABLE = ``;
+const CREATE_MOVIE_KEYWORDS_TABLE = `CREATE TABLE ${MOVIE_KEYWORDS} (
+  movie_id integer NOT NULL,
+  keyword_id integer NOT NULL,
+  PRIMARY KEY (movie_id, keyword_id)
+)`;
 
-const CREATE_MOVIE_PRODUCTION_COMPANIES_TABLE = ``;
+const CREATE_MOVIE_PRODUCTION_COMPANIES_TABLE = `CREATE TABLE ${MOVIE_PRODUCTION_COMPANIES} (
+  movie_id integer NOT NULL,
+  company_id integer NOT NULL,
+  PRIMARY KEY (movie_id, company_id)
+)`;
 
 describe("Insert Combined Data", () => {
   let db: Database;
